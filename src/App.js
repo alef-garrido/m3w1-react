34cfdb2ce911483935b4f6813a10fcc1bc471cb0
alef-garrido <oa.g11@icloud.com>
2022-03-16 14:22:17 -0600

    Initialize Calculator component's constructor w state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Calculator from './components/Calculator';
 
 /* eslint-disable-next-line react/prefer-stateless-function  */
 class App extends React.Component {
+
   render() {
     return (
       <div className="App">
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 /* eslint-disable-next-line react/prefer-stateless-function  */
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+  
   render() {
     const arr = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
     const calcButtons = arr.map((key) => <button type="button" key={key} name={key}>{key}</button>);
