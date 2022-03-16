02678a62957a489a85dc958abd223a48fead2c04
alef-garrido <oa.g11@icloud.com>
2022-03-16 14:34:51 -0600

    Add big.js dependency, handleClick function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Calculator from './components/Calculator';
 
 /* eslint-disable-next-line react/prefer-stateless-function  */
 class App extends React.Component {
-
   render() {
     return (
       <div className="App">
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,17 +3,33 @@ import React from 'react';
 /* eslint-disable-next-line react/prefer-stateless-function  */
 class Calculator extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       total: null,
       next: null,
       operation: null,
     };
+
+    this.handleClick = this.handleChange.bind(this); // keypad click event
   }
-  
+
+  handleClick() {
+    console.log(this.state);
+  }
+
   render() {
     const arr = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
-    const calcButtons = arr.map((key) => <button type="button" key={key} name={key}>{key}</button>);
+    const calcButtons = arr.map((key) => (
+      <button
+        type="button"
+        key={key}
+        name={key}
+        onClick={this.handleClick}
+      >
+        {key}
+      </button>
+    ));
+
     return (
       <div className="calculator--body">
         <input className="calculator--display" placeholder="0" />
