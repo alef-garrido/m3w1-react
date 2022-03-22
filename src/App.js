03959f317ac0b85d38fa87ee6822b255f8de7126
alef-garrido <oa.g11@icloud.com>
2022-03-22 10:46:00 -0600

    Add Routes structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Calculator from './components/Calculator';
+import Navbar from './components/Navbar'
+import Home from './components/pages/Home'
+import CalcPage from './components/pages/CalcPage'
+import Quote from './components/pages/Quote'
 
 /* eslint-disable-next-line react/prefer-stateless-function  */
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='calculator' element={<CalcPage />}/>
+          <Route path='quote' element={<Quote />} />
+        </Routes>
       </div>
     );
   }
